5c6d136df25281da9c7bc8e7d2384efe4b9a679e
贺文婷 <hewt@bjcathay.com>
2019-06-02 20:37:50 +0800

    modify

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,8 +1,31 @@
 <template>
     <div class="login">
       <div class="wrap">
+        <h2>登录</h2>
 
+        <div class="clearfix">
+          <div class="login-img"></div>
 
+          <form class="login-form">
+            <div class="title-bar"></div>
+            <div class="form-item">
+              <label for="">用户名：</label>
+              <input type="text" name="userName" placeholder="请输入用户名">
+            </div>
+
+            <div class="form-item">
+              <label for="">密码：</label>
+              <input type="password" name="password" placeholder="请输入密码">
+            </div>
+
+            <div class="form-item">
+              <label for="">验证码：</label>
+              <input type="text" name="captcha" placeholder="请输入验证码">
+              <span><img src="" alt="验证码" class="captcha"></span>
+              <a href="javascript:;">换一换</a>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
 </template>
--- src/components/login/Register.vue
+++ src/components/login/Register.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="login register">
       <div class="wrap">
+        <h2>注册</h2>
 
+        <div class="clearfix">
+          <div class="login-img"></div>
+
+          <div class="register-form">
+
+          </div>
+        </div>
       </div>
     </div>
 </template>
